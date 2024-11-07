a9c4f69dbac9a37a1cc4f94932861a17fa116f5a
wburt <william.burt@gov.bc.ca>
2024-11-07 14:10:43 -0800

    fix flask app

--- WebMap/Examples/flask_mapLibre/app/app.py
+++ WebMap/Examples/flask_mapLibre/app/app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+    return app
+
+
+app = create_app()
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5000, threaded=True)
