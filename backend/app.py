1c211d2dccab02826b013d6a9a5ed5abc73e2e00
ElocF <cole.folkers@gov.bc.ca>
2024-11-28 09:11:51 -0800

    fixed paths to static, templates and plugins

--- backend/app.py
+++ backend/app.py
@@ -32,9 +32,9 @@ def app_root():
     # Render the main page
     return render_template('index.html')
 
-@app.route('/plugins/<path:filename>')
+@app.route('/templates/plugins/<path:filename>')
 def serve_node_modules(filename):
-    return send_from_directory('.templates/plugins/', filename)
+    return send_from_directory('./templates/plugins', filename)
 
 # WFS proxy route
 @app.route('/wfs<path:endpoint>', methods=['GET', 'POST', 'OPTIONS'])
