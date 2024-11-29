0fc6cb11485971bc061761c7ecc1ff60858f0ff1
ElocF <cole.folkers@gov.bc.ca>
2024-11-29 09:40:27 -0800

    test api script- works

--- WebMap/Examples/leaflet/layer_from_NL.py
+++ WebMap/Examples/leaflet/layer_from_NL.py
@@ -2,23 +2,39 @@
 from dotenv import load_dotenv
 import os 
 import requests
+import json
+import geojson
+import logging 
 
 load_dotenv()
-
 env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),  '../../../backend/.env')
 load_dotenv(env_path)
 
 api_key = os.getenv("NL_API")
-# print(f"Your API key is: {api_key}")
 
-url = "https://native-land.ca/api/index.php"
+# Base URL
+base_url = "https://native-land.ca/api/index.php"
+
+# Parameters for the query
 params = {
-    "maps": "territories",
-    "name": "gitanyow",
-    "key": api_key,  # Replace 'key' with the correct parameter name if needed
+    "maps": "languages,territories",  # Categories to query
+    "name": "gitanyow-laxyip",  # Latitude and Longitude
+    "key": api_key  # API key
 }
 
-response = requests.get(url, params=params)
+# Make the GET request
+response = requests.get(base_url, params=params)
 
-print("Request URL:", response.url)
-print("Response content:", response.text)
+# Check the response status
+if response.status_code == 200:
+    # Parse the JSON response
+    data = response.json()
+    data=data[0]
+else:
+    logging.error(f"Error: {response.status_code}")
+    logging.debug(response.text)
+    
+geojson_data = {
+    "type": "FeatureCollection",
+    "features": [data]
+}
